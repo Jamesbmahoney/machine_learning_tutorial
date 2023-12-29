0ede42534e618176f89756838c2f129790ab4cbe
James Barry <jbm03003@gmail.com>
2023-12-29 13:23:31 -0500

    added undo and save functionality

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -6,15 +6,27 @@ class SketchPad {
     this.canvas.style = `
         background-color:white;
         box-shadow: 0px 0px 10px 2px black;
-        `;
+     `;
     container.appendChild(this.canvas);
+
+    const lineBreak = document.createElement("br");
+    container.appendChild(lineBreak);
+
+    this.undoBtn = document.createElement("button");
+    this.undoBtn.innerHTML = "UNDO";
+    container.appendChild(this.undoBtn);
 
     this.ctx = this.canvas.getContext("2d");
 
-    this.paths = [];
-    this.isDrawing = false;
+    this.reset();
 
     this.#addEventListeners();
+  }
+
+  reset() {
+    this.paths = [];
+    this.isDrawing = false;
+    this.#redraw();
   }
 
   #addEventListeners() {
@@ -31,7 +43,7 @@ class SketchPad {
         this.#redraw();
       }
     };
-    this.canvas.onmouseup = () => {
+    document.onmouseup = () => {
       this.isDrawing = false;
     };
     this.canvas.ontouchstart = (evt) => {
@@ -42,14 +54,23 @@ class SketchPad {
       const loc = evt.touches[0];
       this.canvas.onmousemove(loc);
     };
-    this.canvas.ontouchend = (evt) => {
-      this.canvas.onmouseup();
+    document.ontouchend = () => {
+      document.onmouseup();
+    };
+    this.undoBtn.onclick = () => {
+      this.paths.pop();
+      this.#redraw();
     };
   }
 
   #redraw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     draw.paths(this.ctx, this.paths);
+    if (this.paths.length > 0) {
+      this.undoBtn.disabled = false;
+    } else {
+      this.undoBtn.disabled = true;
+    }
   }
 
   #getMouse = (evt) => {
